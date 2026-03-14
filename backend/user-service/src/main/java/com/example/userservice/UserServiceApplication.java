0f2ec9949fc6c0a48fcfdb2d152db173d15a0686
NgZhiKai <78411240+NgZhiKai@users.noreply.github.com>
2026-03-14 16:49:45 +0800

    test github actions

--- backend/user-service/src/main/java/com/example/userservice/UserServiceApplication.java
+++ backend/user-service/src/main/java/com/example/userservice/UserServiceApplication.java
@@ -1,24 +1,12 @@
 package com.example.userservice;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class UserServiceApplication {
-    private static final Logger LOGGER = LoggerFactory.getLogger(UserServiceApplication.class);
-
-    @Value("${spring.datasource.url:}")
-    private String datasourceUrl;
-
     public static void main(String[] args) {
         SpringApplication.run(UserServiceApplication.class, args);
     }
 
-    @jakarta.annotation.PostConstruct
-    public void logDatasourceUrl() {
-        LOGGER.info("UserService datasource URL (spring.datasource.url) = {}", datasourceUrl);
-    }
 }
